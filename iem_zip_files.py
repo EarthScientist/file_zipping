60a0bb016a3db2b4fa78e3e2aee8d2b3323a720f
Michael Lindgren <lindgren.mike@gmail.com>
2013-01-18 13:09:01 -0900

    made changes to make the darn thing run with the rsds and vap data for the tem model group

--- iem_zip_files.py
+++ iem_zip_files.py
@@ -1,4 +1,4 @@
-from zipfile_infolist import print_info
+from zipfile_infolist import vapint_info
 import zipfile, os, sys
 
 compression = zipfile.ZIP_DEFLATED
@@ -7,7 +7,7 @@ os.chdir("/big_storage/malindgren/AIEM/ZIP")
 
 # here lets create a list of the data we want in each of the zip groups
 year_groups = [range(1901,2009)]
-variables = ["tas","pr"]
+variables = ["rsds","vap"]
 months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
 models = ["cru_TS31"]
 for model in models:
@@ -19,11 +19,11 @@ for model in models:
 			zf = zipfile.ZipFile(model+"_"+v+"_"+str(year_groups[val][0])+".zip", mode='w', allowZip64=True)
 			for year in year_groups[val]:
 				for mon in months:
-					if v == "tas":
-						# filelist.append(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/ALFRESCO_Master_Dataset/ALFRESCO_Model_Input_Datasets/AK_CAN_Inputs/Climate",model,"sresa1b","tas","tas_mean_C_iem_ar4_cccma_cgcm3_1_sresa1b_"+mon+"_"+year+".tif"))
-						zf.write(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/Climate",model,"historical","tas","tas_mean_C_iem_"+model+"_"+mon+"_"+str(year)+".tif"), arcname="tas_mean_C_iem_cru_"+model.upper()+mon+"_"+str(year)+".tif",compress_type=compression)
+					if v == "rsds":
+						# filelist.append(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/ALFRESCO_Master_Darsdset/ALFRESCO_Model_Input_Darsdsets/AK_CAN_Inputs/Climate",model,"sresa1b","rsds","rsds_mean_C_iem_ar4_cccma_cgcm3_1_sresa1b_"+mon+"_"+year+".tif"))
+						zf.write(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/Climate",model,"historical","rsds","rsds_mean_wm-2_iem_"+model+"_"+mon+"_"+str(year)+".tif"), arcname="rsds_mean_wm-2_iem_"+model+"_"+mon+"_"+str(year)+".tif",compress_type=compression)
 					else:
-						# filelist.append(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/ALFRESCO_Master_Dataset/ALFRESCO_Model_Input_Datasets/AK_CAN_Inputs/Climate",model,"sresa1b","pr","pr_total_mm_iem_ar4_cccma_cgcm3_1_sresa1b_"+mon+"_"+year+".tif"))
-						zf.write(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/Climate",model,"historical","pr","pr_total_mm_iem_"+model+"_"+mon+"_"+str(year)+".tif"), arcname="pr_total_mm_iem_cru_"+model.upper()+mon+"_"+str(year)+".tif", compress_type=compression)
+						# filelist.append(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/ALFRESCO_Master_Darsdset/ALFRESCO_Model_Input_Darsdsets/AK_CAN_Inputs/Climate",model,"sresa1b","vap","vap_total_mm_iem_ar4_cccma_cgcm3_1_sresa1b_"+mon+"_"+year+".tif"))
+						zf.write(os.path.join("/big_storage/malindgren/AIEM/ALFRESCO_AIEM_CLIP/Climate",model,"historical","vap","vap_mean_hPa_iem_"+model+"_"+mon+"_"+str(year)+".tif"), arcname="vap_mean_hPa_iem_"+model+"_"+mon+"_"+str(year)+".tif", compress_type=compression)
 			print 'closing'
 			zf.close()

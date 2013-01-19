583ac84315fed8d725e8e1879c47af51c137f510
Michael Lindgren <lindgren.mike@gmail.com>
2013-01-18 15:42:29 -0900

    changed the val in [0,1] to a single element list since we only have a single grouping range

--- iem_zip_files.py
+++ iem_zip_files.py
@@ -11,7 +11,7 @@ months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
 models = ["cru_TS31"]
 for model in models:
 	print model
-	for val in [0,1]:
+	for val in [0]:
 		filelist=[]
 		for v in variables:
 			print 'creating archive'
